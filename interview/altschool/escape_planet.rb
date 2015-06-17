6799f24056b498d6a7e29b33a57b017d9e64330c
George Chen (Air) <sylvanknave@gmail.com>
2015-06-17 09:34:39 -0500

    altschool problem - testing solution

--- interview/altschool/escape_planet.rb
+++ interview/altschool/escape_planet.rb
@@ -2,8 +2,10 @@ require 'net/https'
 require 'json'
 require 'rubygems'
 
+class Escape
+
 # Download via HTTP
-def download( link )
+def self.download( link )
    uri = URI( link )
 
    http = Net::HTTP.new( uri.host, uri.port )
@@ -18,21 +20,21 @@ end
 
 
 # Download JSON via HTTP
-def download_json( link )
+def self.download_json( link )
    data = download(link)
    JSON.parse(data)
 end
 
 
 # Read JSON from file
-def read_json( file_path )
+def self.read_json( file_path )
    JSON.load( IO.read(file_path) )
 end
 
 
 
 # @return true if we exploded at the current node
-def is_dead?(node, blast, asteroids)
+def self.is_dead?(node, blast, asteroids)
    t = node[:t]
    p = node[:p]
 
@@ -40,37 +42,35 @@ def is_dead?(node, blast, asteroids)
 
    # If our dumb-asses reversed into the planet
    if p < 0
+      # puts "Wrecked at #{node} - FROM BEING DUMB"
       dead = true
    end
 
    # If blast hit us
    if t / blast > p
+      # puts "Wrecked at #{node} - FROM BLAST"
       dead = true
    end
 
    # If asteroid collided with us
-   asteroid = asteroids[p - 1] # NOTE that p=1 is first asteroid
-   cycle = asteroid[ 't_per_asteroid_cycle' ]
-   offset = asteroid[ 'offset' ]
-   current_offset = (offset + t) % cycle
-   if 0 == current_offset
-      dead = true
+   if p != 0
+      asteroid = asteroids[p - 1] # NOTE: ignore p=0, first asteroid is p=1
+      cycle = asteroid[ 't_per_asteroid_cycle' ]
+      offset = asteroid[ 'offset' ]
+      current_offset = (offset + t) % cycle
+      if 0 == current_offset
+         # puts "Wrecked at #{node} - FROM ASTEROID"
+         dead = true
+      end
    end
 
    dead
 end
 
-# input = read_json( 'v3_chart.json' )
-input = download_json('https://gist.githubusercontent.com/anonymous/0872aafe0bb179d81bed/raw/11675dcbd240af3362e8e4d63be265cc7b774ab6/chart.json')
-# input = read_json 'v3_chart.json'
-# xputs input
 
-puts input['asteroids'].length
-
-
-# The idea is a depth-first search
-# With priority given to the +1 path
-def find_escape(data)
+# Depth-first search to survive the escape
+# @return an array of the acceleration values needed
+def self.find_escape(data)
 
    asteroids = data['asteroids'] # { a1, a2, ... }
    blast = data['t_per_blast_move'] # int
@@ -87,12 +87,13 @@ def find_escape(data)
    #       p=asteroids.length is the last asteroid
 
    while not stack.empty?
+
       current = stack.shift
       p = current[:p]
 
       if p > safe_distance
          # We have escaped!
-         puts "ESCAPE! #{current}"
+         # puts "ESCAPE! #{current}"
 
          # Stop the search
          stack.clear
@@ -131,6 +132,37 @@ def find_escape(data)
 end
 
 
+def self.benchmark_helper data, length
+
+   input = {
+      't_per_blast_move' => data['t_per_blast_move'],
+      'asteroids' => data['asteroids'][0..length]
+   }
+
+end
+
+def self.benchmark
+   sample = download_json('https://gist.githubusercontent.com/anonymous/0872aafe0bb179d81bed/raw/11675dcbd240af3362e8e4d63be265cc7b774ab6/chart.json')
+   input = Escape.read_json( 'v3_chart.json' )
+
+   require 'benchmark'
+   Benchmark.bm(7) do |x|
+      x.report("Sample:") { Escape.find_escape(sample) }
+
+      length = 5
+      x.report("Input #{length}:") { Escape.find_escape(benchmark_helper(input, length)) }
+
+      length = 50
+      x.report("Input #{length}:") { Escape.find_escape(benchmark_helper(input, length)) }
+
+      length = 100
+      x.report("Input #{length}:") { Escape.find_escape(benchmark_helper(input, length)) }
+
+
+   end
+end
+
+end
 # # Key for a given (time, position, velocity)
 # @PP = 100000
 # @TT = 100000
@@ -139,6 +171,17 @@ end
 # end
 
 
-find_escape(input)
+
+########################################################################
+########################################################################
+
+# Actual data
+input = Escape.read_json( 'v3_chart.json' )
+
+# Test solution speed
+# Escape.benchmark
+
+Escape.find_escape(input)
+
 
 
